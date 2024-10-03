3a64b6d3178212a9c3de871de765970b04e2f8ac
root <root@localhost>
2024-10-03 12:42:41 +0300

    auto publish 1727948560902

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+
+float simpleimterest;,principal, rate,time;
+
+printf ("Enter principal,\n");
+scanf ("%f",& principal);
+
+printf ("Enter the rate,\n");
+scanf ("%f",& rate);
+
+printf ("Enter time,\n");
+scanf ("%f",&time);
+
+si=(principal*rate*time)/100;
+printf ("The simpleinterest is:%.2f",simpleinterest)
+
+
+
+
+
+return 0
+}
